6fbec2d16a4a42d3b1e9f05f89ce6380eccbf210
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-28 22:07:01 +0800

    Created a program that checks if it ends with specified characters without endswith functionhout  check_last_characters_without_endswith_function.py

--- check_last_characters_without_endswith_function.py
+++ check_last_characters_without_endswith_function.py
@@ -1,12 +1,18 @@
-# Prog05. endswith() check if the string end part matches the function parameter. 
-# Create a program that do the same functionality without using endswith() function.
-
 # Ask to enter a text
+text = input("Enter a text: ")
 
 # Ask to enter last characters to check if it ends with the text
+last_character = input("Enter last characters to check: ")
 
 # Check if the text ends with input without using endswith
-
+if len(last_character) > len(text):
+    result = False
+else:
+    result = text[-len(last_character):] == last_character
+  
 # Print the result
-
+if result:
+    print(f"The text ends with '{last_character}'.")
+else:
+    print(f"The text does not end with '{last_character}'.")
 
