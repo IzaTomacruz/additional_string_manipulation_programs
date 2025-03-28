7a0048451bef19977719d52cd4ed849a4f3671a3
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-28 22:24:56 +0800

    Created program that add spaces to the end of the text without using ljust in left_justify_without_ljust_function.py

--- left_justify_without_ljust_function.py
+++ left_justify_without_ljust_function.py
@@ -1,8 +1,12 @@
-# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. 
-# Create a program that do the same functionality without using ljust() function.
-
 # Ask to enter any text 
+text = input("What is your favorite fruit: ")
+width = int(input("Enter the total width: "))
 
 # Add spaces to the end of the text without using ljust
-
+if len(text) < width:
+    text_with_space = text + " " * (width - len(text))
+else:
+    text_with_space = text 
+  
 # Print the result
+print("The", text_with_space, "is my favorite fruit")
