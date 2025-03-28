4f32867da379199d2a135837ee516b0f3371ddb7
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-28 21:23:02 +0800

    Created a program to remove leading spaces on remove_space_without_lstrip_function.py

--- remove_space_without_lstrip_function.py
+++ remove_space_without_lstrip_function.py
@@ -1,8 +1,13 @@
-# Prog01. lstrip() remove the space characters at the beginning of the string. 
-# Create a program that do the same functionality without using lstrip() function.
-
 # Ask to enter any text that has space at the beginning
+text = input("Enter a text: ")
 
 # Remove the space at the beginning of the text without using lstrip
+non_space_indices = [i for i in range(len(text)) if text[i] != " "]
 
+if non_space_indices:
+    text_without_leading_spaces = text[min(non_space_indices):]
+else:
+    text_without_leading_spaces = ""
+  
 # Print the result
+print("Result:", text_without_leading_spaces)
