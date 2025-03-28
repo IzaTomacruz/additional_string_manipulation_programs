7d3fca28f8d593083fc49b94c01d0e71e9bad4d3
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-28 23:03:42 +0800

    Created a program that swap the cases of the text without using swapcase function swap_case_without_swapcase_function.py

--- swap_case_without_swapcase_function.py
+++ swap_case_without_swapcase_function.py
@@ -1,8 +1,16 @@
-# Prog08. swapcase() reverse the casing of each of the character of the string. 
-# Create a program that do the same functionality without using swapcase() function.
-
 # Ask to enter a text
+text = input("Enter a text: ")
 
 # Swap the case of each character without using swapcase
+swapped_case_text = ""
 
+for character in text:
+    if character.islower():  
+        swapped_case_text += character.upper()  
+    elif character.isupper():  
+        swapped_case_text += character.lower()  
+    else:  
+        swapped_case_text += character
+      
 # Print the result
+print("Result:", swapped_case_text)
