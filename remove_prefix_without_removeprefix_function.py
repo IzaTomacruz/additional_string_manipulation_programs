986d4e03a4b97bb848f4120f8db75c8399024183
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-28 21:37:52 +0800

    Created a program to remove prefix in remove_prefix_without_removeprefix_function.py

--- remove_prefix_without_removeprefix_function.py
+++ remove_prefix_without_removeprefix_function.py
@@ -1,8 +1,13 @@
-# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. 
-# Create a program that do the same functionality without using removeprefix() function.
-
 # Ask to enter any text that has a prefix to remove
+text = input("Enter a text: ")
 
 # Remove the prefix from the text without using removeprefix
+prefix = input("Enter the prefix to remove: ")
+
+if text[:len(prefix)] == prefix:
+    without_prefix = text[len(prefix):]
+else:
+    without_prefix = text
 
 # Print the result
+print("Result:", without_prefix)
