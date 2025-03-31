50dd920f22748decf66f923adf9ac3515dcc7641
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 18:47:57 +0800

    Created a program that remove the characters you specified at the end of the string without using the removesuffix function

--- remove_suffix_without_removesuffix_function.py
+++ remove_suffix_without_removesuffix_function.py
@@ -1,10 +1,12 @@
-# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. 
-# Create a program that do the same functionality without using removesuffix() function.
-
 # Ask to enter a text that has a suffix to remove
+text = input("Enter a text: ")
 
 # Ask to enter the suffix to remove  
+suffix = input("Enter the suffix to remove: ")
 
 # Remove the suffix if it matches the end of the string  
-
+if text.endswith(suffix):  
+    text = text[:-len(suffix)] 
+  
 # Print the result 
+print(f"Result: {text}")
