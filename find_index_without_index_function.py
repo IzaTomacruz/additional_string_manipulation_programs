a4fdcc96befba0aaec9c6e8c21c2862376a9e2b3
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 22:10:56 +0800

    redo making a program intended for index

--- find_index_without_index_function.py
+++ find_index_without_index_function.py
@@ -1,23 +1,8 @@
-# Ask to enter ten numbers
-list = []
-print("Enter 10 numbers")
+# Ask to enter text
 
-for i in range(-1, 9):
-    number = int(input(f"Location ({i + 1}): "))
-    list.append(number)
-  
-# Ask to enter a number to find  
-find_number = int(input("Enter a number you want to find: "))
+# Ask to enter a character to find  
 
-# Find the first occurrence of the number 
-location = -1
-for i in range(len(list)):
-    if list[i] == find_number:
-        location = i
-        break
+# Find the first occurrence of the character 
       
 # Print the result 
-if location != -1:
-    print(f"The {find_number} first occured in Location {location}")
-else:
-    print(f"The {find_number} is not in the list")
+
