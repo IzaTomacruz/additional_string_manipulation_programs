e3951a4f49537c877da52468e4458d5d0d978083
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 22:39:12 +0800

    Created the program that returns the first location of the specified character without using the index function

--- find_index_without_index_function.py
+++ find_index_without_index_function.py
@@ -1,8 +1,14 @@
 # Ask to enter text
+text = input("Enter a text: ")
 
 # Ask to enter a character to find  
+character = input("Enter a character to find: ")
 
 # Find the first occurrence of the character 
-      
-# Print the result 
+location = text.find(character)
 
+# Print the result 
+if location != -1:
+    print(location)
+else:
+    print(f'The "{character}" is not in the text')
