609552bcef39d8d0c7f5acc90f3ef3b1a74c0711
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 21:34:38 +0800

    Created program that find the first occurrence of a number without using the index function

--- find_index_without_index_function.py
+++ find_index_without_index_function.py
@@ -1,10 +1,23 @@
-# Prog09. index() return the first location of the function parameter in the string. 
-# Create a program that do the same functionality without using index() function.
-
 # Ask to enter ten numbers
+list = []
+print("Enter 10 numbers")
 
+for i in range(-1, 9):
+    number = int(input(f"Location ({i + 1}): "))
+    list.append(number)
+  
 # Ask to enter a number to find  
+find_number = int(input("Enter a number you want to find: "))
 
 # Find the first occurrence of the number 
-
+location = -1
+for i in range(len(list)):
+    if list[i] == find_number:
+        location = i
+        break
+      
 # Print the result 
+if location != -1:
+    print(f"The {find_number} first occured in Location {location}")
+else:
+    print(f"The {find_number} is not in the list")
