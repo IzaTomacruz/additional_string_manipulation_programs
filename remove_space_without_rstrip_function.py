6b9c5c651a160f5ea686b9b5e64dece750ed83c3
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 18:36:53 +0800

    Created program that remove the space at the end of the string without using the rstrip function

--- remove_space_without_rstrip_function.py
+++ remove_space_without_rstrip_function.py
@@ -1,8 +1,9 @@
-# Prog01. rstrip() remove the space characters at the end of the string. 
-# Create a program that do the same functionality without using rstrip() function.
-
 # Ask to enter a text that has space at the end
+name = input("Enter your name: ")
 
 # Remove the space of the end of the string
-
+while name[-1] == " ":
+    name = name[:-1]  
+  
 # Print the result
+print(f"Hello {name}!")
