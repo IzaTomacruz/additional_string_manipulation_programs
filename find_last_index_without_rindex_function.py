5d69634546f6fc78dc500b90150c7931093b6cd0
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 22:49:13 +0800

    Created a program that returns the last location of the specified character

--- find_last_index_without_rindex_function.py
+++ find_last_index_without_rindex_function.py
@@ -1,10 +1,14 @@
-# Prog10. rindex() return the first location of the function parameter in the string starting from the last character. 
-# Create a program that do the same functionality without using rindex() function.
-
 # Ask to enter text
+text = input("Enter a text: ")
 
 # Ask to enter a text to find  
+character = input("Enter a character to find: ")
 
 # Find the last occurrence of the text 
+location = text.rfind(character)
 
 # Print the result 
+if location != -1:
+    print(f'The "{character}" last occured in {location}')
+else:
+    print(f'The "{character}" is not in the text')
