fbea7f9ba4478e970e0e179ff65ecc422a3179f1
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 19:57:45 +0800

    Create program that checks if the string beginning part matches the function parameter without using the startswith function

--- check_start_of_string_without_startswith_function.py
+++ check_start_of_string_without_startswith_function.py
@@ -1,10 +1,15 @@
-# Prog05. startswith() check if the string beginning part matches the function parameter. 
-# Create a program that do the same functionality without using startswith() function.
-
 # Ask to enter a text  
+text = input("Enter a text: ")
 
 # Ask to enter the prefix to check  
+prefix = input("Enter the prefix to check: ")
 
 # Check if the text starts with the given prefix  
+print("Does it matched?: True or False?") 
 
+if text[:len(prefix)] == prefix:
+    print("Result: True")
+else:
+    print("Result: False")
+  
 # Print true if it matches or false if not
