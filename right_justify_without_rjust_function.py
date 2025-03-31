a54cf17001d1f204bfc704010acd41844ff29922
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 20:45:37 +0800

    Created program that add space characters at the beginning of the string to complete the number of characters specifies in function parameter without using the rjust function

--- right_justify_without_rjust_function.py
+++ right_justify_without_rjust_function.py
@@ -1,10 +1,13 @@
-# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. 
-# Create a program that do the same functionality without using rjust() function.
-
 # Ask to enter a text  
+text1 = input("Enter a text: ")
+text2 = input("Enter another text: ")
 
 # Ask to enter the total width of the final string  
+width = int(input("Enter the total width: "))
 
 # Add spaces at the beginning until it reaches the specified width  
+text1 = " " * (width - len(text1)) + text1
+text2 = " " * (width - len(text2)) + text2
 
 # Print the result  
+print(f"Result: \n'{text1}'\n'{text2}'") 
