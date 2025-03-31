c82a119592076040856faf7c2eb81d52996ce12a
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-31 18:11:19 +0800

    Created a program that asks you to enter 10 names of fruits, and counts how many times you entered the fruit you want to count without using the count function

--- count_occurrences_without_count_function.py
+++ count_occurrences_without_count_function.py
@@ -1,10 +1,22 @@
-# Prog08. count() return how many time the function parameter appear in the string. 
-# Create a program that do the same functionality without using count() function.
-
 # Ask to enter ten fruits
+basket = []
+print("Enter 10 fruits")
 
+for i in range(10):
+    fruit = input(f"Fruit {i + 1}: ")
+    basket.append(fruit)
+  
 # Ask what fruit you want to count
+find_fruit = input("Enter a fruit you want to count: ")
 
 # Count how many times you entered the fruit
+count = 0
+for fruit in basket:
+    if fruit == find_fruit:
+        count += 1
 
 # Print the result  
+if count > 0:
+    print(f"You entered the {find_fruit} {count} times.")
+else:
+    print(f"No fruit named {find_fruit} found")
